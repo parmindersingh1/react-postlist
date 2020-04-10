9473ae5efc4539a0ceda5aa9a6ec954fe6c6002f
Parminder Singh <parminder@datiot.com>
2020-04-10 08:43:40 +0530

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,48 @@
+import _ from "lodash";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+
+  // Promise.all(userIds.map(id =>dispatch(fetchUser(id))));
+};
+
+export const fetchPosts = () => async (dispatch, getState) => {
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatch({
+    type: "FETCH_POSTS",
+    payload: response.data,
+  });
+};
+
+export const fetchUser = (id) => async (dispatch, getState) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({
+    type: "FETCH_USER",
+    payload: response.data,
+  });
+};
+
+//memoized version
+// export const fetchUser = (id) => (dispatch, getState) =>
+//   _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data,
+//   });
+// });
